69d3a6f8f2b7e06100e8e149d26e481b8b3fb80c
Vedanshu Nishad <vedanshun05@gmail.com>
2026-05-10 23:58:36 +0530

    Revert next.config.ts (installOptions is not valid in Next.js 16)

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  installOptions: {
-    legacyPeerDeps: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
