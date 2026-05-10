b701112f95a7ce1f7bfd80a897b241799e5ffb2e
Vedanshu Nishad <vedanshun05@gmail.com>
2026-05-10 23:56:22 +0530

    Add legacy-peer-deps to next.config and strict-peer-deps to .npmrc

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  installOptions: {
+    legacyPeerDeps: true,
+  },
 };
 
 export default nextConfig;
